f062e910c61e6e8be4453e305548640bcbd044b6
Alipio Neto <70041812+alipionetoo@users.noreply.github.com>
2023-06-14 18:02:11 -0300

    tirei as threads

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from threading import Thread
-import os
+import os 
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-users = {}
-
-def handle_connect(sid):
-    users[sid] = True
-
-def user_thread(sid):
-    while users.get(sid, False):
-        message = socketio.get_session(sid).receive()
-        if message is None:
-            break
-        else:
-            emit('broadcast', message, broadcast=True, include_self=False)
 
 @app.route('/')
 def index():
@@ -25,15 +11,11 @@ def index():
 
 @socketio.on('connect')
 def handle_connect_event():
-    sid = request.sid
-    users[sid] = True
-    thread = Thread(target=user_thread, args=(sid,))
-    thread.start()
+    emit('connected', {'data': 'Connected'})
 
 @socketio.on('disconnect')
 def handle_disconnect_event():
-    sid = request.sid
-    users.pop(sid, None)
+    emit('disconnected', {'data': 'Disconnected'})
 
 @socketio.on('message')
 def handle_message(message):
