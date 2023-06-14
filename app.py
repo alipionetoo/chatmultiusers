03ec853bb506d219f3a35556503f8445cbb43790
Alipio Neto <70041812+alipionetoo@users.noreply.github.com>
2023-06-14 09:52:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from threading import Thread
+import os 
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+users = {}
+
+def handle_connect(sid):
+    users[sid] = Thread(target=user_thread, args=(sid,))
+    users[sid].start()
+
+def user_thread(sid):
+    while True:
+        message = socketio.wait(sid)
+        if message is None:
+            break
+        else:
+            emit('broadcast', message, broadcast=True, include_self=False)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('client.html')
 
 @socketio.on('connect')
-def handle_connect():
+def handle_connect_event():
     emit('connected', {'data': 'Connected'})
 
 @socketio.on('disconnect')
-def handle_disconnect():
+def handle_disconnect_event():
     emit('disconnected', {'data': 'Disconnected'})
 
 @socketio.on('message')
 def handle_message(message):
-    emit('message', {'data': message['data']}, broadcast=True)
+    emit('broadcast', message, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=int(os.getenv("PORT")), allow_unsafe_werkzeug=True)
