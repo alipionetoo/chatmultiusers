f27d5bbdcd7ce7c8ad837fa9eb1fce6317cd2f0d
Alipio Neto <70041812+alipionetoo@users.noreply.github.com>
2023-06-14 18:30:24 -0300

    vomemata

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import os
-import eventlet
+from threading import Thread
+import os 
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='eventlet')
+socketio = SocketIO(app)
+
+users = {}
+
+def handle_connect(sid):
+    users[sid] = Thread(target=user_thread, args=(sid,))
+    users[sid].start()
+
+def user_thread(sid):
+    while True:
+        message = socketio.wait(sid)
+        if message is None:
+            break
+        else:
+            emit('broadcast', message, broadcast=True, include_self=False)
 
 @app.route('/')
 def index():
@@ -23,4 +37,4 @@ def handle_message(message):
     emit('broadcast', message, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.getenv("PORT")))
+    socketio.run(app, host='0.0.0.0', port=int(os.getenv("PORT")), allow_unsafe_werkzeug=True)
